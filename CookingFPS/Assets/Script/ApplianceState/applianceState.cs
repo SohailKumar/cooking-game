aa304afc4e6a67b0f3426552b9ae33ef953caa68
Ji <greenyjwk@gmail.com>
2022-02-05 17:06:16 -0500

    Fix applianceState

--- CookingFPS/Assets/Script/ApplianceState/applianceState.cs
+++ CookingFPS/Assets/Script/ApplianceState/applianceState.cs
@@ -15,7 +15,7 @@ public class applianceState : monoBehavior
     }
     public void beingCooked()
     {
-        this.beingCooked = 
+ 
     }
     
  
@@ -28,54 +28,13 @@ public class applianceState : monoBehavior
     changeTheMenu(string nextMenu){
         this.mealsbeingCooked = nextMenu;
     }
-    public void BeDead()
-    {
-        MarioPowerupState.BeDead();
-        MarioMovementState.BeDead();
 
-    }
     
     public void Update()
     {
-        MarioSprite.Update();
-        Physics.Update();
-        if((this.location.X > GameObjectManager.Instance.EndOfLevelXPosition -MarioUtil.FlagpoleBuffer && this.location.X < GameObjectManager.Instance.EndOfLevelXPosition + MarioUtil.FlagpoleBuffer) && !hasCompletedLevel)
-        {
-            hasCompletedLevel = true;
-            ScoringSystem.Instance.AddPointsForRestTime();
-            if (MarioPowerupState is NormalMarioPowerupState)
-                GameObjectManager.Instance.GameObjectList.SetSingleton(typeof(IMario),new SlideDownFlagDecorator(this, new Vector2(this.location.X, MarioUtil.HeightOfFloor)));
-            else
-                GameObjectManager.Instance.GameObjectList.SetSingleton(typeof(IMario), new SlideDownFlagDecorator(this, new Vector2(this.location.X, MarioUtil.HeightOfFloor-MarioUtil.MarioHeight)));
-
-            atTheEnd = true;
-        }
-        if (isOnPipe)
-        {
-            location.Y++;
-        }
+        
+        
     }
     
     
-    public void SetIsLand(bool land)
-    {
-        if (land)
-            Island = true;
-        else
-            Island = false;
-    }
-         
-
-    public bool IsJump()
-    {
-        if (MarioMovementState is LeftJumpingMarioMovementState||
-            MarioMovementState is RightJumpingMarioMovementState)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
 }
