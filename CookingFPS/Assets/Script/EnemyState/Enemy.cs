da0683a951c0628046ea8671c18e60bbee9d5285
SohailK <sohail.kumar03@gmail.com>
2022-02-06 05:56:09 -0500

    Added thing to protect, Game Over

--- CookingFPS/Assets/Script/EnemyState/Enemy.cs
+++ CookingFPS/Assets/Script/EnemyState/Enemy.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy:MonoBehaviour{ 
-	[SerializeField] private int hp; 
-	//private String battle_cry; 
-	//private String is_hit_sound; 
-	//private String dying_sound; 
-	public void TakeDamage(int dmg)
+	[SerializeField] private int hp;
+	[SerializeField] private float step;
+	public GameObject target;
+    public GameManager gm;
+    //private String battle_cry; 
+    //private String is_hit_sound; 
+    //private String dying_sound; 
+
+    public void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Protect");
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    public void TakeDamage(int dmg)
     {
 		Debug.Log("HP: " + hp + ", DMG: " + dmg);
 		hp -= dmg;
 		if(hp <= 0)
         {
 			Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step * Time.deltaTime);
+        transform.LookAt(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Protect"))
+        {
+            gm.GameOver();
         }
     }
 }
--- CookingFPS/Assets/Script/GameState/GameManager.cs
+++ CookingFPS/Assets/Script/GameState/GameManager.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void NextLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Restart(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
